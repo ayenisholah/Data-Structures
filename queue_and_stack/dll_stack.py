e36fc45986a99346ced2944bdb897f97afee4f63
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-14 15:48:16 +0100

    increase the size of the storage

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,10 +6,11 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        # Increase the size of the storage
+        self.size += 1
 
     def pop(self):
         pass
