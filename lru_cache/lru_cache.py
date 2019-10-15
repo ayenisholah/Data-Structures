ec50826d9455b93a41a27c87a052d59b6f719595
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-15 15:46:15 +0100

    Least Recently Used

--- lru_cache/lru_cache.py
+++ lru_cache/lru_cache.py
@@ -1,3 +1,8 @@
+from doubly_linked_list import DoublyLinkedList, ListNode
+import sys
+sys.path.append('../doubly_linked_list')
+
+
 class LRUCache:
     """
     Our LRUCache class keeps track of the max number of nodes it
@@ -6,8 +11,22 @@ class LRUCache:
     order, as well as a storage dict that provides fast access
     to every node stored in the cache.
     """
+
     def __init__(self, limit=10):
-        pass
+        self.size = 0
+        self.storage = DoublyLinkedList()
+        """
+            once the cache is holding the max number of entries,
+            if a new entry is to be inserted,
+            another pre-existing entry needs to be evicted from the cache.
+            Once you access an item/get, it moves to the front of the list
+            The least recently used is gonna be at the tail of the list
+            The data structure to use is an hashtable for fast look ups
+            Advantages of a doubly linked list is that you can remove and add an item in constant time
+            # PROBLEMS
+            1. How do we know where the least recently used item is.
+            2. How do we remove items quickly
+        """
 
     """
     Retrieves the value associated with the given key. Also
@@ -16,8 +35,18 @@ class LRUCache:
     Returns the value associated with the key or None if the
     key-value pair doesn't exist in the cache.
     """
+
     def get(self, key):
-        pass
+        current_node = ListNode(value)
+        # when you get an item, set the key to 1
+        # Move the item to the head of the list
+        if (self.storage == None):
+            # Storage is empty, return -1
+            return -1
+        self.storage.move_to_front(current_node)
+        self.key = 1
+        return current_node.value
+        
 
     """
     Adds the given key-value pair to the cache. The newly-
@@ -29,5 +58,11 @@ class LRUCache:
     want to overwrite the old value associated with the key with
     the newly-specified value.
     """
+
     def set(self, key, value):
-        pass
+        node = value
+
+        if node == None:
+            
+
+
